8477fe9b9f7369b44507a1fc10d9c0e97c630f0e
Anar Seyf <aseyf@apple.com>
2016-05-10 08:36:17 -0700

    Uglified and minified

--- brr.sh
+++ brr.sh
@@ -1,2 +1,6 @@
 
-browserify index.js -o bundle.js -t [ babelify --presets [ es2015 react ] ]
+browserify index.js \
+    -t [ babelify --presets [ es2015 react ] ] \
+    -t uglifyify \
+    -d -p [minifyify --map bundle.min.js.map --output bundle.min.js.map] \
+    > bundle.min.js
